1be36da43a7338b1bbe9ce84fde544179797f701
bara <164997496+melkibara@users.noreply.github.com>
2024-05-28 14:56:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-9d219159-d490-40f4-b55d-7f27cb5971f3/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-9618904a-fb9a-4fc6-9238-b5db8f9b7cd0/)
 
 
 echo "$curl_output" >> Update.tsx
